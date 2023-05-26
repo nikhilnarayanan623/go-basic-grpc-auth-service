89c00357fcda34b8c0a5900a2a92b731493b6469
nikhil623 <nikhilnarayanan623@gmail.com>
2023-05-26 09:12:35 +0530

    user login signup and validation on token completed

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nikhilnarayanan623/go-basic-grpc-auth-service
 go 1.20
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/spf13/viper v1.15.0
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.1
--- pkg/token/jwt.go
+++ pkg/token/jwt.go
@@ -1,1 +1,58 @@
 package token
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/nikhilnarayanan623/go-basic-grpc-auth-service/pkg/config"
+)
+
+type jwtAuth struct {
+	jwtSecret string
+}
+
+type JwtClaims struct {
+	jwt.RegisteredClaims
+	UserId uint32
+}
+
+func NewJwtTokenAuth(cfg *config.Config) TokenAuth {
+	return &jwtAuth{
+		jwtSecret: cfg.JWTSecret,
+	}
+}
+
+func (c *jwtAuth) GenerateToken(tokenReq TokenRequest) (tokenString string, err error) {
+
+	claims := &JwtClaims{
+		UserId: tokenReq.UserId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(tokenReq.ExpirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err = token.SignedString([]byte(c.jwtSecret))
+	return
+}
+func (c *jwtAuth) VerifyToken(tokenString string) (TokenResponse, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &JwtClaims{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("invalid signing method on token")
+		}
+		return []byte(c.jwtSecret), nil
+	})
+
+	if err != nil {
+		return TokenResponse{}, fmt.Errorf("faild to parse token string to token")
+	}
+
+	claims, ok := token.Claims.(*JwtClaims)
+	if !ok {
+		return TokenResponse{}, fmt.Errorf("faild to parse the token")
+	}
+
+	return TokenResponse{
+		UserId: claims.UserId,
+	}, nil
+}
